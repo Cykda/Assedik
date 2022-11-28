2f8fc032d2f326f29a196b23d5a69a3b17fb99d0
letheodufutur <117894470+letheodufutur@users.noreply.github.com>
2022-11-28 21:19:19 +0100

    Update moves.c

--- src/moves.c
+++ src/moves.c
@@ -7,15 +7,17 @@
 void move(plateau* p, pion pawn,int *pNBB, int *pNBR)
 {
 
-    p->plateau[pawn.pos.y][pawn.pos.x].couleur = pawn.couleur;
-    /*if (pawn.couleur==0)
+    p->plateau[pawn.pos.y][pawn.pos.x].info.couleur = pawn.info.couleur;
+    if (pawn.info.couleur==WHITE)
     {
         *pNBB=*pNBB-1;
+        pawn.info.quantite=*pNBB;
     }
-    if (pawn.couleur==1)
+    if (pawn.info.couleur==RED)
     {
         *pNBR=*pNBR-1;
-    }*/
+        pawn.info.quantite=*pNBR;
+    }
 
 }
 
@@ -23,22 +25,24 @@ void move(plateau* p, pion pawn,int *pNBB, int *pNBR)
 void directMove(plateau* p, int color, int x, int y)
 {
     //couleur c = color;
-    p->plateau[y][x].couleur = color;
+    p->plateau[y][x].info.couleur = color;
 }
 
-pion inputPawn(char* color_message, char* pos_message)
+pion inputPawn(char* color_message, char* pos_message,int tour)
 {
-
+    int quitter=0;
     pion pawn;
     printf("%s", color_message);
-    scanf("%d", &pawn.couleur);
-    if(pawn.couleur == -2)
+    scanf("%d", &quitter);
+    if(quitter == 1)
     {
+        pawn.info.couleur=-2;
         pawn.pos.x = -1;
         pawn.pos.y = -1;
 
         return pawn;
     }
+    pawn.info.couleur=tour;
     printf("%s", pos_message);
     scanf("%d %d", &pawn.pos.x, &pawn.pos.y);
 
