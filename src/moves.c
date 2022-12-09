36c08625cad521d6bd80e85da908a02b96777516
letheodufutur <117894470+letheodufutur@users.noreply.github.com>
2022-12-09 15:26:22 +0100

    Update moves.c

--- src/moves.c
+++ src/moves.c
@@ -4,7 +4,7 @@
 
 
 
-void move(plateau* p, pion pawn,int *pNBB, int *pNBR)
+void placement(plateau* p, pion pawn,int *pNBB, int *pNBR)
 {
 
     p->plateau[pawn.pos.y][pawn.pos.x].info.couleur = pawn.info.couleur;
@@ -47,4 +47,111 @@ pion inputPawn(char* color_message, char* pos_message,int tour)
     scanf("%d %d", &pawn.pos.x, &pawn.pos.y);
 
     return pawn;
+}
+
+int choix_deplacement(plateau* p, pion pawn)
+{
+    int choix;
+    do
+    {
+        printf("Vous avez le choix entre deplacer ce pion (1) ou effectuer le coup special (2). Faites votre choix: ");
+        scanf("%d",&choix);
+        fflush(stdin);
+    } while(choix!=1 && choix!=2);
+    if (choix==1)
+    {
+        printf("\nVous avez choisi de deplacer le pion.\n");
+        return 1;
+    }
+    if (choix==2)
+    {
+        printf("\nVous  avez choisis d'utiliser le coup special. Votre pion est retiré du plateau, vous pourrez le replacer au prochain tour");
+        p->plateau[pawn.pos.y][pawn.pos.x].info.couleur=NONE;
+        return 2;
+    }
+}
+
+int coup_special(plateau* p,int tour,int N)
+{
+    int x,y;
+    while(true)
+    {
+        printf("\nDonnez les coordonnees souhaitees pour placer le pion retire (X,Y)");
+        scanf("%d %d",&x,&y);
+        fflush(stdin);
+        if (y<0 || x<0 || y>(N-1) || x>(N-1))
+        {
+            printf("\nLa case ne convient pas : hors du plateau\n");
+            continue;
+        }
+        else if (y>=0 && x>=0 && y<=(N-1) && x<=(N-1))
+        {
+            if (p->plateau[y][x].info.couleur!=NONE)
+            {
+                printf("\nLa case ne convient pas : case deja ocupee\n");
+                continue;
+            }
+            else if (p->plateau[y][x].info.couleur==NONE)
+            {
+                if (tour==1)
+                {
+                    p->plateau[y][x].info.couleur=RED;
+                    return 0;
+                }
+                else if (tour==0)
+                {
+                    p->plateau[y][x].info.couleur=WHITE;
+                    return 0;
+                }
+            }
+        }
+    }
+}
+
+int deplacement_pion(plateau* p,pion pawn,int tour,int N)
+{
+    int x,y;
+    while (true)
+    {
+        printf("\nDonnez les coordonnees ou vous voulez deplacer le pion (X,Y): ");
+        scanf("%d %d",&x,&y);
+        fflush(stdin);
+        if (y<0 || x<0 || y>(N-1) || x>(N-1))
+        {
+            printf("\nLa case ne convient pas : hors du plateau\n");
+            continue;
+        }
+        else if (x>(pawn.pos.x)+1 || x<(pawn.pos.x)-1 || y>(pawn.pos.y)+1 || y<(pawn.pos.y)-1)
+        {
+            printf("\nERREUR: La case n'est pas adjacente a celle du pion\n");
+            continue;
+        }
+        else if (x<=(pawn.pos.x)+1 && x>=(pawn.pos.x)-1 && y<=(pawn.pos.y)+1 && y>=(pawn.pos.y)-1)
+        {
+            if (p->plateau[y][x].info.couleur==WHITE)
+            {
+                printf("\nERREUR, la case est deja occupee par un pion blanc\n");
+            }
+            else if (p->plateau[y][x].info.couleur==RED)
+            {
+                printf("\nERREUR, la case est deja occupee par un pion rouge\n");
+            }
+            else if (p->plateau[y][x].info.couleur==NONE)
+            {
+                printf("\nBravo, la case convient parfaitement !\n");
+                if (tour==1)
+                {
+                    p->plateau[y][x].info.couleur=RED;
+                    p->plateau[pawn.pos.y][pawn.pos.x].info.couleur=NONE;
+                    return 0;
+                }
+                else if (tour==0)
+                {
+                    p->plateau[y][x].info.couleur=WHITE;
+                    p->plateau[pawn.pos.y][pawn.pos.x].info.couleur=NONE;
+                    return 0;
+                }
+            }
+        }
+    }
 }
