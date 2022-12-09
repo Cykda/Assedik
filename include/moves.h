875bbdcd91c43792b9bcd74deeaa620eb28ff678
letheodufutur <117894470+letheodufutur@users.noreply.github.com>
2022-12-09 15:35:40 +0100

    Update moves.h

--- include/moves.h
+++ include/moves.h
@@ -6,8 +6,11 @@
 
 
 
-void move(plateau* p, pion pawn,int *pNBB, int *pNBR);
+void placement(plateau* p, pion pawn,int *pNBB, int *pNBR);
 void directMove(plateau* p, int color, int x, int y);
 pion inputPawn(char* color_message, char* pos_message,int tour);
+int choix_deplacement(plateau* p,pion pawn);
+int coup_special(plateau* p,int tour,int N);
+int deplacement_pion(plateau* p,pion pawn,int tour,int N);
 
 #endif /*MOVES_H*/
