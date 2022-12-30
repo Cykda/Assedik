d57bc657480ee6e61be292c9e19a3853c206fe69
Bonjourrrrr <97249109+Bonjourrrrr@users.noreply.github.com>
2022-12-30 15:51:02 +0100

    Update moves.h

--- include/moves.h
+++ include/moves.h
@@ -5,7 +5,7 @@
 
 
 
-
+void placement(plateau* p, pion pawn,int *pNBB, int *pNBR);
 void move(plateau* p, pion pawn);
 void directMove(plateau* p, int color, int x, int y);
 pion inputPawn(char* color_message, char* pos_message);
